2f6c7ef6fe225fcfc0a7ea59745a39a80db87ac1
scarthy <n00220387@iadt.ie>
2026-04-25 03:05:17 +0100

    deploy1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://siofracarthy.github.io/portfolio-repo/',
+    base: '/portfolio-repo/',
   vite: {
     plugins: [tailwindcss()],
   },
